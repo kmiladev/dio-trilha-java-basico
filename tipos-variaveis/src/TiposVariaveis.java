c8ea5e5bf3e46bfd89d46dc3341d501886cecda7
Camila <kmiladoors@gmail.com>
2024-05-31 22:27:08 -0300

    exercicio de tipo variavel e constante

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //se a gente escrever os tipos assim:
+        //short numeroCurto = 1;
+        //int numeroNormal = numeroCurto;
+        //short numeroCurto = numeroNormal; //esse erro ocorre pois o numeroNormal tem precisao maior do que o numeroCurto.
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto = (short) numeroNormal; // realizar uma adaptacao para colocar um valor com possível precisao maior pára uma precisao menor vc tem ciencia. casting
+    
+        // variável
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        // constante
+        final double VALOR_DE_PI = 3.14; // variável constante não pode mais ser alterado seu valor.
+
+
+        //estudar tipos primitivos
+        // estudar classes String
+
     }
 }
