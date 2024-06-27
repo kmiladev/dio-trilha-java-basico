fe7d23993b6bdab2215b3984cbfe9002c11b124f
Camila <kmiladoors@gmail.com>
2024-06-26 21:37:31 -0300

    Desafio Contador

--- ControleDeFluxo/src/ExemploExcecao.java
+++ ControleDeFluxo/src/ExemploExcecao.java
@@ -9,7 +9,7 @@ public class ExemploExcecao {
     // a classe runtime determina que a exceção que pode acontecer já está sendo tratada pelo java.
     // o erro pode acontecer mas só pode ser tratado após ocorrer.
 
-    /*Podemos ainda etstar as exceções usando outro método:
+    /*Podemos ainda testar as exceções usando outro método:
      * import java.text.Number Format;
      * public class ExemploExcecao;
      * public static void main(String[] args){
@@ -31,7 +31,7 @@ public class ExemploExcecao {
      * } catch (ParseEsception e) {
      * e.printStackTrace();
      * }
-     * consultando essa esceção vemos que trata-se de uma exception.
+     * consultando essa exceção vemos que trata-se de uma exception.
      * 
      * checked e unchecked significa que vc deve tratar o erro imediatamente, já o runtime o erro pode ocorrer para depois tratar.
      */
--- ControleDeFluxo/src/FormatadorCepExemplo.java
+++ ControleDeFluxo/src/FormatadorCepExemplo.java
@@ -4,16 +4,17 @@ public class FormatadorCepExemplo {
         String cepFormatado = formatarCep("23765064"); // lancando o cep nesse formato ele já exige o try catch por conta de ser uma classe de excecao que é estendida de exception.
     } // não se trata de um erro e sim um tratamento de excecao.
     // criação do método de formatar cep
-static String formatarCep(String cep) throws CepInvalidoException{ // condição para lançar o exception
-    if(cep.length() != 8)
-        throw new CepInvalidoException(); // a condicao que vai ser lançado
+
+    static String formatarCep(String cep) throws CepInvalidoException{ // condição para lançar o exception
+        if(cep.length() != 8)
+            throw new CepInvalidoException(); // a condicao que vai ser lançado
         // simulando um cep formatado
-        return "23.765-064";  // se o cep fpo diferente de 8 caracteres vai printar CepInvalidoException;
+            return "23.765-064";  // se o cep fpo diferente de 8 caracteres vai printar CepInvalidoException;
 }
     /*O recurso acima com o tratamento solicitado pelo programa ficaria:
      * try{
      *      String cepFormatado = formatarCep("23765064");
-     *      // System.out.println(cepFprmatado
+     *      // System.out.println(cepFormatado
      * } catch (CepInvalidoException e) {
      *      e.printStackTrace();
      * }
