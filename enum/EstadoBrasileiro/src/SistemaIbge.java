a7171250d9af13241e4066ec4d505ac688896aab
Camila <kmiladoors@gmail.com>
2024-07-10 09:20:12 -0300

    enum, teste

---
+++ enum/EstadoBrasileiro/src/SistemaIbge.java
@@ -0,0 +1,16 @@
+public class SistemaIbge {
+    public static void main(String[] args) {
+        
+        for(EstadoBrasileiro e: EstadoBrasileiro.values()){
+            System.out.println(e.getSigla() + " - " + e.getNome());
+        }
+
+        EstadoBrasileiro eb = EstadoBrasileiro.SAO_PAULO;
+        
+        System.out.println(eb.getNome());
+        System.out.println(eb.getSigla());
+        System.out.println(eb.getNomeMaiusculo());
+        System.out.println(eb.getIbge());
+    }
+    
+}
